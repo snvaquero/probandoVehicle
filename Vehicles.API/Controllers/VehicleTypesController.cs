d74d6b9b9b52ee86aa97eb2f0dd6e6bbd346dccd
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2021-08-13 14:00:22 -0500

    Vehicle types

---
+++ Vehicles.API/Controllers/VehicleTypesController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using Vehicles.API.Data;
+using Vehicles.API.Data.Entities;
+
+namespace Vehicles.API.Controllers
+{
+    public class VehicleTypesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public VehicleTypesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.VehicleTypes.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Description")] VehicleType vehicleType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vehicleType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicleType);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            VehicleType vehicleType = await _context.VehicleTypes.FindAsync(id);
+            if (vehicleType == null)
+            {
+                return NotFound();
+            }
+            return View(vehicleType);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Description")] VehicleType vehicleType)
+        {
+            if (id != vehicleType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vehicleType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VehicleTypeExists(vehicleType.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicleType);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            VehicleType vehicleType = await _context.VehicleTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vehicleType == null)
+            {
+                return NotFound();
+            }
+
+            _context.VehicleTypes.Remove(vehicleType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VehicleTypeExists(int id)
+        {
+            return _context.VehicleTypes.Any(e => e.Id == id);
+        }
+    }
+}
--- Vehicles.API/Data/DataContext.cs
+++ Vehicles.API/Data/DataContext.cs
@@ -11,11 +11,14 @@ namespace Vehicles.API.Data
 
         public DbSet<DocumentType> DocumentTypes { get; set; }
 
+        public DbSet<VehicleType> VehicleTypes { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<DocumentType>().HasIndex(dt => dt.Description).IsUnique();
+            modelBuilder.Entity<VehicleType>().HasIndex(dt => dt.Description).IsUnique();
         }
     }
 }
