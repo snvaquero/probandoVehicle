788609bfefc725005a483c445386cd0d5556fea0
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2021-09-08 10:40:31 -0500

    Add controllers for brands, document types & vehicle types

--- Vehicles.API/Controllers/API/BrandsController.cs
+++ Vehicles.API/Controllers/API/BrandsController.cs
@@ -14,43 +14,43 @@ namespace Vehicles.API.Controllers.API
     [ApiController]
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("api/[controller]")]
-    public class ProceduresController : ControllerBase
+    public class BrandsController : ControllerBase
     {
         private readonly DataContext _context;
 
-        public ProceduresController(DataContext context)
+        public BrandsController(DataContext context)
         {
             _context = context;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Procedure>>> GetProcedures()
+        public async Task<ActionResult<IEnumerable<Brand>>> GetBrands()
         {
-            return await _context.Procedures.OrderBy(x => x.Description).ToListAsync();
+            return await _context.Brands.OrderBy(x => x.Description).ToListAsync();
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Procedure>> GetProcedure(int id)
+        public async Task<ActionResult<Brand>> GetBrand(int id)
         {
-            Procedure procedure = await _context.Procedures.FindAsync(id);
+            Brand brand = await _context.Brands.FindAsync(id);
 
-            if (procedure == null)
+            if (brand == null)
             {
                 return NotFound();
             }
 
-            return procedure;
+            return brand;
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutProcedure(int id, Procedure procedure)
+        public async Task<IActionResult> PutBrand(int id, Brand brand)
         {
-            if (id != procedure.Id)
+            if (id != brand.Id)
             {
                 return BadRequest();
             }
 
-            _context.Entry(procedure).State = EntityState.Modified;
+            _context.Entry(brand).State = EntityState.Modified;
 
             try
             {
@@ -75,14 +75,14 @@ namespace Vehicles.API.Controllers.API
         }
 
         [HttpPost]
-        public async Task<ActionResult<Procedure>> PostProcedure(Procedure procedure)
+        public async Task<ActionResult<Brand>> PostBrand(Brand brand)
         {
-            _context.Procedures.Add(procedure);
+            _context.Brands.Add(brand);
 
             try
             {
                 await _context.SaveChangesAsync();
-                return CreatedAtAction("GetProcedure", new { id = procedure.Id }, procedure);
+                return CreatedAtAction("GetBrand", new { id = brand.Id }, brand);
             }
             catch (DbUpdateException dbUpdateException)
             {
@@ -102,15 +102,15 @@ namespace Vehicles.API.Controllers.API
         }
 
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteProcedure(int id)
+        public async Task<IActionResult> DeleteBrand(int id)
         {
-            Procedure procedure = await _context.Procedures.FindAsync(id);
-            if (procedure == null)
+            Brand brand = await _context.Brands.FindAsync(id);
+            if (brand == null)
             {
                 return NotFound();
             }
 
-            _context.Procedures.Remove(procedure);
+            _context.Brands.Remove(brand);
             await _context.SaveChangesAsync();
 
             return NoContent();
