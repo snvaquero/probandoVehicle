3f92bf7bed37df1f23eef0f2b858fe2f83bb396b
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2021-09-13 20:45:32 -0500

    POST & PUT in User Api Controller

--- Vehicles.API/Controllers/API/UsersController.cs
+++ Vehicles.API/Controllers/API/UsersController.cs
@@ -2,25 +2,35 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Vehicles.API.Data;
 using Vehicles.API.Data.Entities;
+using Vehicles.API.Helpers;
+using Vehicles.API.Models.Requests;
 using Vehicles.Common.Enums;
+using Vehicles.Common.Models;
 
 namespace Vehicles.API.Controllers.API
 {
     [ApiController]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
         private readonly DataContext _context;
+        private readonly IUserHelper _userHelper;
+        private readonly IMailHelper _mailHelper;
+        private readonly IBlobHelper _blobHelper;
 
-        public UsersController(DataContext context)
+        public UsersController(DataContext context, IUserHelper userHelper, IMailHelper mailHelper, IBlobHelper blobHelper)
         {
             _context = context;
+            _userHelper = userHelper;
+            _mailHelper = mailHelper;
+            _blobHelper = blobHelper;
         }
 
         [HttpGet]
@@ -59,6 +69,106 @@ namespace Vehicles.API.Controllers.API
             }
 
             return user;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(UserRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            DocumentType documentType = await _context.DocumentTypes.FindAsync(request.DocumentTypeId);
+            if (documentType == null)
+            {
+                return BadRequest("El tipo de documento no existe.");
+            }
+
+            User user = await _userHelper.GetUserAsync(request.Email);
+            if (user != null)
+            {
+                return BadRequest("Ya existe un usuario registrado con ese correo.");
+            }
+
+            Guid imageId = Guid.Empty;
+            if (request.Image != null && request.Image.Length > 0)
+            {
+                imageId = await _blobHelper.UploadBlobAsync(request.Image, "users");
+            }
+
+            user = new User
+            {
+                Address = request.Address,
+                Document = request.Document,
+                DocumentType = documentType,
+                Email = request.Email,
+                FirstName = request.FirstName,
+                ImageId = imageId,
+                LastName = request.LastName,
+                PhoneNumber = request.PhoneNumber,
+                UserName = request.Email,
+                UserType = UserType.User,
+            };
+
+            await _userHelper.AddUserAsync(user, "123456");
+            await _userHelper.AddUserToRoleAsync(user, user.UserType.ToString());
+
+            string myToken = await _userHelper.GenerateEmailConfirmationTokenAsync(user);
+            string tokenLink = Url.Action("ConfirmEmail", "Account", new
+            {
+                userid = user.Id,
+                token = myToken
+            }, protocol: HttpContext.Request.Scheme);
+
+            _mailHelper.SendMail(request.Email, "Vehicles - Confirmación de cuenta", $"<h1>Vehicles - Confirmación de cuenta</h1>" +
+                $"Para habilitar el usuario, " +
+                $"por favor hacer clic en el siguiente enlace: </br></br><a href = \"{tokenLink}\">Confirmar Email</a>");
+
+            return Ok(user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(string id, UserRequest request)
+        {
+            if (id != request.Id)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            DocumentType documentType = await _context.DocumentTypes.FindAsync(request.DocumentTypeId);
+            if (documentType == null)
+            {
+                return BadRequest("El tipo de documento no existe.");
+            }
+
+            User user = await _userHelper.GetUserAsync(Guid.Parse(request.Id));
+            if (user == null)
+            {
+                return BadRequest("No existe ese usuario.");
+            }
+
+            Guid imageId = user.ImageId;
+            if (request.Image != null && request.Image.Length > 0)
+            {
+                imageId = await _blobHelper.UploadBlobAsync(request.Image, "users");
+            }
+
+            user.Address = request.Address;
+            user.Document = request.Document;
+            user.DocumentType = documentType;
+            user.FirstName = request.FirstName;
+            user.ImageId = imageId;
+            user.LastName = request.LastName;
+            user.PhoneNumber = request.PhoneNumber;
+
+            await _userHelper.UpdateUserAsync(user);
+            return NoContent();
         }
     }
 }
