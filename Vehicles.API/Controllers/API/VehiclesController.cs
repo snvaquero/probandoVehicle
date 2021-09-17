b59199f1a47147c7b964a4a2fffc0f3aa0d1a15e
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2021-09-16 19:06:44 -0500

    Add vehicles api controller

---
+++ Vehicles.API/Controllers/API/VehiclesController.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Vehicles.API.Data;
+using Vehicles.API.Data.Entities;
+using Vehicles.API.Helpers;
+using Vehicles.API.Models.Requests;
+
+namespace Vehicles.API.Controllers.API
+{
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/[controller]")]
+    public class VehiclesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly IBlobHelper _blobHelper;
+        private readonly IUserHelper _userHelper;
+
+        public VehiclesController(DataContext context, IBlobHelper blobHelper, IUserHelper userHelper)
+        {
+            _context = context;
+            _blobHelper = blobHelper;
+            _userHelper = userHelper;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVehicle(int id, VehicleRequest request)
+        {
+            if (id != request.Id)
+            {
+                return BadRequest();
+            }
+
+            VehicleType vehicleType = await _context.VehicleTypes.FindAsync(request.VehicleTypeId);
+            if (vehicleType == null)
+            {
+                return BadRequest("El tipo de vehículo no existe.");
+            }
+
+            Brand brand = await _context.Brands.FindAsync(request.BrandId);
+            if (brand == null)
+            {
+                return BadRequest("Esa marca no existe.");
+            }
+
+            User user = await _userHelper.GetUserAsync(request.UserId);
+            if (user == null)
+            {
+                return BadRequest("Esa usuario no existe.");
+            }
+
+            Vehicle vehicle = await _context.Vehicles.FindAsync(request.Id);
+            if (vehicle == null)
+            {
+                return BadRequest("Esa vehículo no existe.");
+            }
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                return NoContent();
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe un vehículo con esa placa.");
+                }
+                else
+                {
+                    return BadRequest(dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Vehicle>> PostVehicle(VehicleRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            VehicleType vehicleType = await _context.VehicleTypes.FindAsync(request.VehicleTypeId);
+            if (vehicleType == null)
+            {
+                return BadRequest("El tipo de vehículo no existe.");
+            }
+
+            Brand brand = await _context.Brands.FindAsync(request.BrandId);
+            if (brand == null)
+            {
+                return BadRequest("Esa marca no existe.");
+            }
+
+            User user = await _userHelper.GetUserAsync(request.UserId);
+            if (user == null)
+            {
+                return BadRequest("Esa usuario no existe.");
+            }
+
+            Vehicle vehicle = await _context.Vehicles.FirstOrDefaultAsync(x => x.Plaque.ToUpper() == request.Plaque.ToUpper());
+            if (vehicle != null)
+            {
+                return BadRequest("Ya existe un vehículo con esa placa.");
+            }
+
+            Guid imageId = Guid.Empty;
+            if (request.Image != null && request.Image.Length > 0)
+            {
+                imageId = await _blobHelper.UploadBlobAsync(request.Image, "users");
+            }
+
+            vehicle = new Vehicle
+            {
+                Brand = brand,
+                Color = request.Color,
+                Line = request.Line,
+                Model = request.Model,
+                Plaque = request.Plaque,
+                Remarks = request.Remarks,
+                User = user,
+                VehicleType = vehicleType,
+                VehiclePhotos = new List<VehiclePhoto>
+                {
+                    new VehiclePhoto { ImageId = imageId }
+                },
+            };
+
+            _context.Vehicles.Add(vehicle);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetVehicle", new { id = vehicle.Id }, vehicle);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            Vehicle vehicle = await _context.Vehicles
+                .Include(x => x.VehiclePhotos)
+                .Include(x => x.Histories)
+                .ThenInclude(x => x.Details)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            _context.Vehicles.Remove(vehicle);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Vehicles.API/Models/Requests/VehicleRequest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vehicles.API.Models.Requests
+{
+    public class VehicleRequest
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int VehicleTypeId { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int BrandId { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Range(1900, 3000, ErrorMessage = "Valor de módelo no válido.")]
+        public int Model { get; set; }
+
+        [RegularExpression(@"[a-zA-Z]{3}[0-9]{2}[a-zA-Z0-9]", ErrorMessage = "Formato de placa incorrecto.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "El campo {0} debe tener {1} carácteres.")]
+        public string Plaque { get; set; }
+
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Line { get; set; }
+
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Color { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string UserId { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Remarks { get; set; }
+
+        public byte[] Image { get; set; }
+    }
+}
